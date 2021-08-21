ae33473db38c142f2719f2a66fbffc40c8320049
mehmet19034 <mehmetbeken1903@gmail.com>
2021-08-22 01:02:09 +0300

    3.ders ödevi

---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+            isimler.Add(23, "Mehmet");
+            isimler.Add(44, "Hamza");
+            foreach (var item in isimler.value)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(isimler.Count);
+                
+        }
+    }
+}
--- KampOdevler/Program.cs
+++ KampOdevler/Program.cs
@@ -6,7 +6,7 @@ namespace KampOdevler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
